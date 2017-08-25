2c5c08feda1c85080ea7c1e5fa19f30315fc0080
unknown <orhan.hajderovic@hotmail.com>
2017-08-25 14:12:25 +0200

    Added login and signup folder/pages + routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,10 +1,11 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 import { SellBooksComponent } from './books/sell-books/sell-books.component';
 import { BuyBooksComponent } from './books/buy-books/buy-books.component';
 import { BuyBookListComponent } from './books/buy-books/buy-book-list.component';
 import { SellBookListComponent } from './books/sell-books/sell-book-list.component';
 import { BuyBookComponent } from './books/buy-books/buy-book.component';
 import { SellBookComponent } from './books/sell-books/sell-book.component';
-import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header.component';
 import { routing } from './app.routing';
 import { BookComponent } from './books/books.component';
@@ -18,14 +19,15 @@ import { AppComponent } from "./app.component";
         AppComponent,
         BookComponent,
         HeaderComponent,
-        AuthComponent,
         BookComponent,
         SellBookComponent,
         BuyBookComponent,
         SellBookListComponent,
         BuyBookListComponent,
         BuyBooksComponent,
-        SellBooksComponent
+        SellBooksComponent,
+        LoginComponent,
+        SignupComponent
     ],
     imports: [BrowserModule, routing],
     bootstrap: [AppComponent]
--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,3 +1,5 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 import { AuthComponent } from './auth/auth.component';
 import { BuyBookComponent } from './books/buy-books/buy-book.component';
 import { SellBookComponent } from './books/sell-books/sell-book.component';
@@ -7,7 +9,8 @@ const APP_ROUTES: Routes = [
     {path: '', redirectTo: '/', pathMatch: 'full'},
     {path: 'sell-book', component: SellBookComponent},
     {path: 'buy-book', component: BuyBookComponent},
-    {path: 'auth', component: AuthComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent}
 ]
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
